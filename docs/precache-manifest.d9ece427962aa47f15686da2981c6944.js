029ec88a309082f128190b9541114806e79e7051
sabareesh sunku <sabareesh.sunku@espn.com>
2020-08-29 02:09:55 +0530

    added docs as github pages

---
+++ docs/precache-manifest.d9ece427962aa47f15686da2981c6944.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4032b79cef052b07109f028e5a6c3dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2659d88a80f7d2ddc98d",
+    "url": "/static/css/main.1c36fb61.chunk.css"
+  },
+  {
+    "revision": "b9455ccd9675434c4de8",
+    "url": "/static/js/2.ee162741.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ee162741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2659d88a80f7d2ddc98d",
+    "url": "/static/js/main.8aac06c2.chunk.js"
+  },
+  {
+    "revision": "d1425d6b969d122ed6f1",
+    "url": "/static/js/runtime-main.cbf9c502.js"
+  }
+]);
